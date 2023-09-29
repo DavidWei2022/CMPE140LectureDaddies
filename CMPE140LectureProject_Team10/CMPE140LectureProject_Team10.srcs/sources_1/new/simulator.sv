e20f7651078b0e8601896166005e5c8cc9b16437
Matthew Malvini <legosrule1999@gmail.com>
2023-09-28 20:39:22 -0700

    I made the simulator.sv file (you're welcome)

---
+++ CMPE140LectureProject_Team10/CMPE140LectureProject_Team10.srcs/sources_1/new/simulator.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09/28/2023 08:36:51 PM
+// Design Name: 
+// Module Name: simulator
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module simulator(
+
+    );
+endmodule
